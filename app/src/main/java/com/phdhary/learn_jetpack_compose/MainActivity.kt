1f11dd1ad91fc99390893d45768c9c90c8ee5036
Hary <phdhary@gmail.com>
2022-02-03 17:10:55 +0700

    image card

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -3,23 +3,29 @@ package com.phdhary.learn_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.phdhary.learn_jetpack_compose.ui.theme.myAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,26 +43,59 @@ class MainActivity : ComponentActivity() {
 )
 @Composable
 fun HomeScreen() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceAround,
-        modifier = Modifier
-            .fillMaxSize()
-            .border(
-                border = BorderStroke(12.dp, color = Color.Magenta)
+    val painter = painterResource(R.drawable.wolf)
+    val desc = "Mr I"
+    val title = "Good Boi"
+
+    Box(modifier = Modifier.fillMaxWidth(0.5f)) {
+        ImageCard(
+            painter = painter,
+            contentDescription = desc,
+            title = title
+        )
+    }
+}
+
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp,
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
             )
-            .padding(12.dp)
-            .border(
-                shape = RoundedCornerShape(corner = CornerSize(percent = 10)),
-                border = BorderStroke(12.dp, color = Color.Red)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f,
+                        )
+                    )
+            )
+            Text(
+                title,
+                style = TextStyle(fontSize = 16.sp, color = Color.White),
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(12.dp)
             )
 
-            .background(
-                color = Color.Cyan,
-            ),
-    ) {
-        Text("hello")
-        Text("what")
-        Text("is up")
+        }
     }
 }

ca34106e73d0fc879f552a7ba68f29e32b903b08
Hary <phdhary@gmail.com>
2022-02-03 17:44:08 +0700

    add fonts and learn textstyle

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -5,13 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,18 +16,63 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.phdhary.learn_jetpack_compose.ui.theme.myAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(resId = R.font.lexend_bold),
+        )
         setContent {
-            HomeScreen()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xff101010))
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline,
+                )
+            }
         }
     }
 }
@@ -47,11 +87,16 @@ fun HomeScreen() {
     val desc = "Mr I"
     val title = "Good Boi"
 
-    Box(modifier = Modifier.fillMaxWidth(0.5f)) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(12.dp)
+    ) {
         ImageCard(
             painter = painter,
             contentDescription = desc,
-            title = title
+            title = title,
+            modifier = Modifier.clickable { }
         )
     }
 }
@@ -65,8 +110,9 @@ fun ImageCard(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier.fillMaxWidth(),
         shape = RoundedCornerShape(15.dp),
+        modifier = modifier
+            .fillMaxWidth(),
         elevation = 5.dp,
     ) {
         Box(modifier = Modifier.height(200.dp)) {

39c35e19a131c36f838025fab292f073fcb7cad9
Hary <phdhary@gmail.com>
2022-02-03 20:52:28 +0700

    learn external state

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -5,39 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColorBox(Modifier.fillMaxSize())
+            val color = remember {
+                mutableStateOf(Color.Cyan)
+            }
+            Column(modifier = Modifier.fillMaxSize()) {
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize(),
+                ) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+            }
         }
     }
 }
@@ -46,22 +43,21 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Showd() {
-    ColorBox(modifier = Modifier.fillMaxSize())
+//    ColorBox(modifier = Modifier.fillMaxSize())
 }
 
 @Composable
-fun ColorBox(modifier: Modifier) {
-    val color = remember {
-        mutableStateOf(Color.Green)
-    }
+fun ColorBox(modifier: Modifier, updateColor: (Color) -> Unit) {
     Box(
         modifier = modifier
-            .background(color.value)
+            .background(Color.LightGray)
             .clickable {
-                color.value = Color(
-                    Random.nextInt(),
-                    Random.nextInt(),
-                    Random.nextInt(),
+                updateColor(
+                    Color(
+                        Random.nextInt(),
+                        Random.nextInt(),
+                        Random.nextInt(),
+                    )
                 )
             }
     )

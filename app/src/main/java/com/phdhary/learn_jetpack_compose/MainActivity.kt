d71df6b24415e8583b9021ae39318a0ba03ee040
Hary <phdhary@gmail.com>
2022-02-03 20:44:04 +0700

    learn to use state to change color of box

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -29,119 +31,38 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fontFamily = FontFamily(
-            Font(resId = R.font.lexend_bold),
-        )
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xff101010))
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ) {
-                            append("J")
-                        }
-                        append("etpack ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ) {
-                            append("C")
-                        }
-                        append("ompose")
-                    },
-                    modifier = Modifier.align(Alignment.Center),
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.Underline,
+            ColorBox(Modifier.fillMaxSize())
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun Showd() {
+    ColorBox(modifier = Modifier.fillMaxSize())
+}
+
+@Composable
+fun ColorBox(modifier: Modifier) {
+    val color = remember {
+        mutableStateOf(Color.Green)
+    }
+    Box(
+        modifier = modifier
+            .background(color.value)
+            .clickable {
+                color.value = Color(
+                    Random.nextInt(),
+                    Random.nextInt(),
+                    Random.nextInt(),
                 )
             }
-        }
-    }
-}
-
-@Preview(
-    showBackground = true,
-    showSystemUi = true,
-)
-@Composable
-fun HomeScreen() {
-    val painter = painterResource(R.drawable.wolf)
-    val desc = "Mr I"
-    val title = "Good Boi"
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth(0.5f)
-            .padding(12.dp)
-    ) {
-        ImageCard(
-            painter = painter,
-            contentDescription = desc,
-            title = title,
-            modifier = Modifier.clickable { }
-        )
-    }
-}
-
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        shape = RoundedCornerShape(15.dp),
-        modifier = modifier
-            .fillMaxWidth(),
-        elevation = 5.dp,
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f,
-                        )
-                    )
-            )
-            Text(
-                title,
-                style = TextStyle(fontSize = 16.sp, color = Color.White),
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(12.dp)
-            )
-
-        }
-    }
+    )
 }

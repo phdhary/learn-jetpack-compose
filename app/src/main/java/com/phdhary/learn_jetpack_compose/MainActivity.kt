ede9eb35ec302ca109d7e1869f6f811fadd68009
Hary <phdhary@gmail.com>
2022-02-03 21:49:53 +0700

    textfield,button,scaffold&snackbar

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -5,35 +5,63 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val color = remember {
-                mutableStateOf(Color.Cyan)
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf("")
             }
-            Column(modifier = Modifier.fillMaxSize()) {
-                ColorBox(
-                    Modifier
-                        .weight(1f)
-                        .fillMaxSize(),
-                ) {
-                    color.value = it
-                }
-                Box(
+            val scope = rememberCoroutineScope()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                     modifier = Modifier
-                        .background(color.value)
-                        .weight(1f)
                         .fillMaxSize()
-                )
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        textFieldState,
+                        label = {
+                            Text("Enter your name")
+                        },
+                        singleLine = true,
+                        visualTransformation = PasswordVisualTransformation(),
+                        onValueChange = {
+                            textFieldState = it
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                            }
+                        }) {
+                        Text("Show Snackbar")
+                    }
+                }
             }
         }
     }
@@ -44,21 +72,4 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Showd() {
 //    ColorBox(modifier = Modifier.fillMaxSize())
-}
-
-@Composable
-fun ColorBox(modifier: Modifier, updateColor: (Color) -> Unit) {
-    Box(
-        modifier = modifier
-            .background(Color.LightGray)
-            .clickable {
-                updateColor(
-                    Color(
-                        Random.nextInt(),
-                        Random.nextInt(),
-                        Random.nextInt(),
-                    )
-                )
-            }
-    )
 }

04bd1cc7dd924697cded7165410938aad934854e
Hary <phdhary@gmail.com>
2022-02-04 07:12:05 +0700

    simple size animation

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.phdhary.learn_jetpack_compose
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.produceState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 
@@ -20,35 +22,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            val counter = produceState(initialValue = 0) {
-                delay(3000L)
-                value = 10
-            }
+            var sizeState by remember { mutableStateOf(200.dp) }
+            val size by animateDpAsState(targetValue = sizeState)
 
-            if (counter.value % 5 == 0 && counter.value > 0) {
-                LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
-                    scaffoldState.snackbarHostState.showSnackbar("Hello")
-                }
-            }
-
-            Scaffold(
-                scaffoldState = scaffoldState,
-                floatingActionButton = {
-                    FloatingActionButton(onClick = { }) {
-                        Icon(Icons.Rounded.Add, contentDescription = "")
-                    }
-                }
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(Color.Cyan),
+                contentAlignment = Alignment.Center
             ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Text(
-                        counter.value.toString(), fontSize = 30.sp
-                    )
+                Column {
+                    Button(onClick = { sizeState += 50.dp }) {
+                        Text("Expand")
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Button(onClick = { sizeState -= 50.dp }) {
+                        Text("Shrink")
+                    }
+
                 }
             }
         }

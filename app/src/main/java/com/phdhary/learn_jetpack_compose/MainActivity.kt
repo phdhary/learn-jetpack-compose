9528a3e9281049c735559bf4c465f176c2f13a80
Hary <phdhary@gmail.com>
2022-02-04 21:12:43 +0700

    circular progress bar with canvas

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -1,61 +1,129 @@
 package com.phdhary.learn_jetpack_compose
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.Button
+import androidx.compose.material.Slider
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeState by remember { mutableStateOf(200.dp) }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-                spring(Spring.DampingRatioHighBouncy)
-            )
-
-            val infiniteTransition = rememberInfiniteTransition()
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Magenta,
-                targetValue = Color.Cyan,
-                animationSpec = infiniteRepeatable(
-                    animation = tween(durationMillis = 2000),
-                    repeatMode = RepeatMode.Reverse,
-                )
-            )
-
             Box(
-                modifier = Modifier
-                    .size(size)
-                    .background(color),
+                modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                Column {
-                    Button(onClick = { sizeState += 50.dp }) {
-                        Text("Expand")
-                    }
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Button(onClick = { sizeState -= 50.dp }) {
-                        Text("Shrink")
-                    }
-
-                }
+                CircularProgressBar(percentage = 0.8f, number = 100)
             }
         }
+    }
+}
+
+
+@Preview(
+    showSystemUi = true
+)
+@Composable
+fun TestPrev() {
+    var percentage by remember {
+        mutableStateOf(0f)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressBar(
+            percentage = percentage,
+            number = 100
+        )
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 30.dp)
+                .align(Alignment.BottomCenter)
+                .fillMaxHeight(0.4f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = { percentage = 0f }) {
+                Text("Reset")
+            }
+            Slider(
+                value = percentage,
+                onValueChange = {
+                    percentage = it
+                },
+            )
+
+        }
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0,
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(
+            modifier = Modifier.size(radius * 2f)
+        ) {
+            drawArc(
+                color,
+                -90f,
+                360 * curPercentage.value,
+                false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Butt)
+            )
+        }
+        Text(
+            text = (
+                    curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold,
+        )
+
     }
 }

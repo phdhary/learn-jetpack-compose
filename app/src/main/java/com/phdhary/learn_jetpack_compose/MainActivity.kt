a59f90fc3b4ddda6ff2807e2efaf9a94a2ecf128
Hary <phdhary@gmail.com>
2022-02-05 21:59:07 +0700

    music knob using custom motion event

--- app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/phdhary/learn_jetpack_compose/MainActivity.kt
@@ -1,154 +1,164 @@
 package com.phdhary.learn_jetpack_compose
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Slider
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.roundToInt
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var percentage by remember {
-                mutableStateOf(0f)
-            }
             Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xff101010))
             ) {
-                CircularProgressBar(
-                    percentage = percentage,
-                    number = 100
-                )
-                Column(
+                Row(
                     modifier = Modifier
-                        .padding(horizontal = 30.dp)
-                        .align(Alignment.BottomCenter)
-                        .fillMaxHeight(0.4f),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
+                        .padding(30.dp)
                 ) {
-                    Button(onClick = { percentage = 0f }) {
-                        Text("Reset")
+                    var volume by remember {
+                        mutableStateOf(0f)
                     }
-                    Slider(
-                        value = percentage,
-                        onValueChange = {
-                            percentage = it
-                        },
+                    val barCount = 20
+                    MusicKnob(
+                        modifier = Modifier
+                            .size(100.dp)
+                    ) {
+                        volume = it
+                    }
+                    Spacer(modifier = Modifier.width(20.dp))
+                    VolumeBar(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(30.dp),
+                        activeBars = (barCount * volume).roundToInt(),
+                        barCount = barCount
                     )
+
 
                 }
             }
+
         }
     }
 }
 
-
-@Preview(
-    showSystemUi = true
-)
 @Composable
-fun TestPrev() {
-    var percentage by remember {
+fun VolumeBar(
+    modifier: Modifier = Modifier,
+    activeBars: Int = 0,
+    barCount: Int = 10
+) {
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = modifier,
+    ) {
+        val barWidth = remember {
+            constraints.maxWidth / (2f * barCount)
+        }
+        Canvas(modifier = modifier) {
+            for (i in 0 until barCount) {
+                drawRoundRect(
+                    color = if (i in 0..activeBars) Color.Green else DarkGray,
+                    topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                    size = Size(barWidth, constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius(0f)
+                )
+            }
+        }
+
+    }
+
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableStateOf(limitingAngle)
+    }
+
+    var touchX by remember {
         mutableStateOf(0f)
     }
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        CircularProgressBar(
-            percentage = percentage,
-            number = 100
-        )
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 30.dp)
-                .align(Alignment.BottomCenter)
-                .fillMaxHeight(0.4f),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = { percentage = 0f }) {
-                Text("Reset")
+    var touchY by remember {
+        mutableStateOf(0f)
+    }
+    var centerX by remember {
+        mutableStateOf(0f)
+    }
+    var centerY by remember {
+        mutableStateOf(0f)
+    }
+    Image(
+        painter = painterResource(id = R.drawable.music_knob),
+        contentDescription = "Music Knob",
+        modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
             }
-            Slider(
-                value = percentage,
-                onValueChange = {
-                    percentage = it
-                },
-            )
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+                val height = centerY - touchY
+                val base = centerX - touchX
+                val radianAngle = atan2(height, base)
+                val degreeAngle = radianAngle * (180 / PI).toFloat()
 
-        }
-    }
-}
-
-@Composable
-fun CircularProgressBar(
-    percentage: Float,
-    number: Int,
-    fontSize: TextUnit = 28.sp,
-    radius: Dp = 50.dp,
-    color: Color = Color.Green,
-    strokeWidth: Dp = 8.dp,
-    animDuration: Int = 300,
-    animDelay: Int = 0,
-) {
-    var animationPlayed by remember {
-        mutableStateOf(false)
-    }
-    val curPercentage = animateFloatAsState(
-        targetValue = if (animationPlayed) percentage else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = animDelay
-        )
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (degreeAngle !in -limitingAngle..limitingAngle) {
+                            val fixedAngle = if (degreeAngle in -180f..-limitingAngle) {
+                                360f + degreeAngle
+                            } else {
+                                degreeAngle
+                            }
+                            rotation = fixedAngle
+                            val percent = (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percent)
+                            true
+                        } else false
+                    }
+                    else -> false
+                }
+            }
+            .rotate(rotation)
     )
-
-
-
-    LaunchedEffect(key1 = true) {
-        animationPlayed = true
-    }
-
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.size(radius * 2f)
-    ) {
-        Canvas(
-            modifier = Modifier.size(radius * 2f)
-        ) {
-            drawArc(
-                color,
-                -90f,
-                360 * curPercentage.value,
-                false,
-                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Butt)
-            )
-        }
-        Text(
-            text = (
-                    curPercentage.value * number).toInt().toString(),
-            color = Color.Black,
-            fontSize = fontSize,
-            fontWeight = FontWeight.Bold,
-        )
-
-    }
 }
